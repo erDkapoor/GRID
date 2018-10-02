642c86e43beb9160d606e9460af43ff30001474b
erDkapoor <40963349+erDkapoor@users.noreply.github.com>
2018-10-02 23:10:22 +0530

    Add files via upload

---
+++ batch_cleaner.bat
@@ -0,0 +1,14 @@
+@echo off
+rem   just kills stray local chromedriver.exe instances.
+rem   useful if you are trying to clean your project, and your ide is complaining.
+rem   useful if you are trying to change drivers exe path in your project directory, and your ide is complaining
+rem   useful if you are trying to upgrade driver version in your project directory, and your ide is complaining
+taskkill /im chromedriver.exe /f
+
+
+@echo off
+rem   just kills stray local chrome.exe instances.
+rem   useful if you are trying to clean your project, and your ide is complaining.
+rem   useful if you are trying to change drivers exe path in your project directory, and your ide is complaining
+rem   useful if you are trying to upgrade driver version in your project directory, and your ide is complaining
+taskkill /im chrome.exe /f
